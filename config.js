ca7e9826641e59e8c2d8e97b050937e1708fadcc
Karen <kareneth@users.noreply.github.com>
2020-01-01 02:28:46 -0500

    add github to website bio

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ module.exports = {
       facebook: '',
       telegram: '',
       twitter: '',
-      github: '',
+      github: 'kareneth',
       rss: '',
       vkontakte: '',
       linkedin: 'karengshih',
